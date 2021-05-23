2aff562781237d1163063d47c7fa65b39e554b8f
keimos <keithmosleyjr@gmail.com>
2021-05-23 13:15:41 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+def carpet_cleaning_service(amount_paid):
+    if amount_paid == 6:
+        print("You have selected Platinum Service")
+        print("4 bedroom and 1 hallway Power Clean")
+        print("Carpet Shampoo")
+        print("Deep Rinse")
+        print("Steam Non-Chemical Dry")
+
+    if amount_paid == 10:
+        print("You have selected Basic Service")
+        print("2 bedroom Basic Clean")
+        print("Deep Rinse")
+
+carpet_cleaning_service(10)
